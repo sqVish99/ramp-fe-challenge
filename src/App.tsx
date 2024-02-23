743e81f4487197a5df58fb5dd5ccf6cd53d0c944
sqVish99 <vmharish@asu.edu>
2024-02-22 23:09:06 -0700

    BugFix - 5

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ export function App() {
   const { data: transactionsByEmployee, ...transactionsByEmployeeUtils } = useTransactionsByEmployee()
   const [isLoading, setIsLoading] = useState(false)
   const [accumulatedTransactions, setAccumulatedTransactions] = useState<any>([]);
+  const [isTransactionsLoading, setIsTransactionsLoading] = useState(false)
 
   useEffect(() => {
     if (paginatedTransactions?.data) {
@@ -27,19 +28,14 @@ export function App() {
     }
   }, [transactionsByEmployee]);
 
-  const transactions = useMemo(
-    () => paginatedTransactions?.data ?? transactionsByEmployee ?? null,
-    [paginatedTransactions, transactionsByEmployee]
-  )
-
   const loadAllTransactions = useCallback(async () => {
     setIsLoading(true)
     transactionsByEmployeeUtils.invalidateData()
 
     await employeeUtils.fetchAll()
-    await paginatedTransactionsUtils.fetchAll()
-
     setIsLoading(false)
+    
+    await paginatedTransactionsUtils.fetchAll()
   }, [employeeUtils, paginatedTransactionsUtils, transactionsByEmployeeUtils])
 
   const loadTransactionsByEmployee = useCallback(

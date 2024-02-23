7cd4e0779bf0bcf798c434cd16a8b9e707114008
sqVish99 <vmharish@asu.edu>
2024-02-22 23:51:35 -0700

    BugFix - 7

--- src/components/Transactions/index.tsx
+++ src/components/Transactions/index.tsx
@@ -5,16 +5,17 @@ import { TransactionPane } from "./TransactionPane"
 import { SetTransactionApprovalFunction, TransactionsComponent } from "./types"
 
 export const Transactions: TransactionsComponent = ({ transactions }) => {
-  const { fetchWithoutCache, loading } = useCustomFetch()
+  const { fetchWithCache, clearCacheByEndpoint, loading } = useCustomFetch()
 
   const setTransactionApproval = useCallback<SetTransactionApprovalFunction>(
     async ({ transactionId, newValue }) => {
-      await fetchWithoutCache<void, SetTransactionApprovalParams>("setTransactionApproval", {
+      await fetchWithCache<void, SetTransactionApprovalParams>("setTransactionApproval", {
         transactionId,
         value: newValue,
       })
+      clearCacheByEndpoint(['transactionsByEmployee','paginatedTransactions']);
     },
-    [fetchWithoutCache]
+    [fetchWithCache, clearCacheByEndpoint]
   )
 
   if (transactions === null) {
